0e4503aa4011a2cf4df42dfb1e8dd906e393900a
tmartinez114 <tmartinez114@gmail.com>
2019-11-09 18:25:21 -0500

    completed 15:00

---
+++ javascript/05Functions/05-4laugh.js
@@ -0,0 +1,18 @@
+// Directions:
+// Write an anonymous function expression that stores a function in a variable called "laugh" and outputs the number of "ha"s that you pass in as an argument.
+
+//     laugh(3);
+// Returns: hahaha!
+
+var laugh = function (num) {
+    var string = "";
+    for (i = 0; i < +num; i++) {
+        string += "ha"
+    }
+    return string + "!";
+}
+
+console.log(laugh(10));
+
+// Time: 15:00
+// Grade: 3
